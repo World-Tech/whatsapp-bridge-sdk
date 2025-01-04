b352c504d4a47c60cbc1f953bbb61ce1bd1ee422
jemanuelpo <jpachecho@libgot.com>
2025-01-04 02:19:46 -0300

    Add auto-generated DTO models for whatsapp-bridge API
    Introduce auto-generated TypeScript and JavaScript DTOs for modeling data structures within the whatsapp-bridge API. These files should not be manually edited as they sync with the OpenAPI specification.

---
+++ dist/models/index.js
@@ -0,0 +1,31 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !exports.hasOwnProperty(p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+__exportStar(require("./api-request-user-update-password-dto"), exports);
+__exportStar(require("./api-response-auth-dto"), exports);
+__exportStar(require("./api-response-user-dto"), exports);
+__exportStar(require("./attributes"), exports);
+__exportStar(require("./auth-dto"), exports);
+__exportStar(require("./contacts-response-dto"), exports);
+__exportStar(require("./data-dto"), exports);
+__exportStar(require("./data-unauthorized-dto"), exports);
+__exportStar(require("./inline-response503"), exports);
+__exportStar(require("./inline-response503-info"), exports);
+__exportStar(require("./message-dto"), exports);
+__exportStar(require("./role-dto"), exports);
+__exportStar(require("./status-dto"), exports);
+__exportStar(require("./tokens-dto"), exports);
+__exportStar(require("./unauthorized-dto"), exports);
+__exportStar(require("./unauthorized-token-dto"), exports);
+__exportStar(require("./user-detail-dto"), exports);
+__exportStar(require("./user-update-password-dto"), exports);
+__exportStar(require("./whatsapp-contact-dto"), exports);
